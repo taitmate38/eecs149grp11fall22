fc9a1263cd4ec3b7b632ae101fc49edd43c9df25
Andy Fang <andyfang486@berkeley.edu>
2022-12-15 21:57:05 -0800

    bumper added

--- src/carfw/main.cpp
+++ src/carfw/main.cpp
@@ -21,6 +21,11 @@
 #define M1_0 26
 #define M1_1 27
 #define M1_2 12
+#define turret_x_min 820
+#define turret_x_max 3400
+
+#define turret_y_min 550
+#define turret_y_max 2840
 
 #define AIM_THRESHOLD 150
 
@@ -37,9 +42,12 @@ const int steerChannel = 0;
 const int resolution = 12;
 
 
-static int MODULES[6] = {240, 600, 885, 1255, 1445, 1725};
+static int MODULES[7] = {240, 600, 885, 2700, 1445, 1725, 4000};
 
 static LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+static float turret_x = 1600;
+static float turret_y = 1600;
 
 int idt(int pin) {
   int identity = analogRead(pin);
@@ -48,7 +56,7 @@ int idt(int pin) {
     return -1;
   }
   int closest = 9999;
-  for (int i = 0; i < 6; i += 1)
+  for (int i = 0; i < 7; i += 1)
   {
     int check = abs(identity - MODULES[i]);
     if (check < closest)
@@ -57,104 +65,43 @@ int idt(int pin) {
       idModule = i;
     }
   }
+  Serial.print("ID MODULE: ");
   Serial.println(idModule);
+  Serial.print("ANALOG READING: ");
+  Serial.println(identity);
+  Serial.printf("PIN NUMBER: %d \n", pin);
   return idModule;
 }
 
-void handle_modules(int tilt_x, int tilt_y, int c, int z, int idt0, int idt1) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  switch (idt0)
-  {
-  case 0:
-    lcd.print("DIST");
-    //DISTANCE MODULE
-    float duration, cm, inches;
-    pinMode(M0_1, OUTPUT);
-    pinMode(M0_2, INPUT);
-    digitalWrite(M0_1, LOW);
-    delayMicroseconds(5);
-    digitalWrite(M0_1, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(M0_1, LOW);
+void handle_modules(int tilt_x, int tilt_y, int joy_x, int joy_y, int c, int z, int idtPin, int gpio0, int gpio1, int gpio2) {
+  int tilt_x_channel = 1;
+  int tilt_y_channel = 2;
+  float increment_x, increment_y;
+  int bump;
 
-    pinMode(M0_2, INPUT);
-    duration = pulseIn(M0_2, HIGH);
-
-    inches = (duration / 2) / 74;
-
-    Serial.print("INCHES: ");
-    Serial.println(inches);
-    if (inches < 5 && inches > 0) {
-      // idle for half a second
-      digitalWrite(MTR1_1, LOW);
-      digitalWrite(MTR1_2, LOW);
-
-      delay(500);
-
-      //back up for 1/2 second
-      ledcWrite(steerChannel, 2000);
-      digitalWrite(MTR1_1, LOW);
-      digitalWrite(MTR1_2, HIGH);
-
-      delay(500);
-      
-      //return to idle
-      digitalWrite(MTR1_1, LOW);
-      digitalWrite(MTR1_2, LOW);
-      Serial.println("STOPPPPP");
-    }
-
-    break;
-  case 1:
-    // code block
-    break;
-  case 2:
-    // code block
-    break;
-  case 3:
-    break;
-  case 4:
-    lcd.print("LASER");
-    // LASER
-    pinMode(M0_0, OUTPUT);
-    if (c)
-    {
-      digitalWrite(M0_0, HIGH);
-    }
-    else
-    {
-      digitalWrite(M0_0, LOW);
-    }
-    break;
-  case 5:
-    break;
-  case -1:
-    Serial.println("No Module plugged in");
-    break;
-  default:
-    // code block
-    Serial.println("Unrecognized Module");
+  if (idtPin == IDT1) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+  } else {
+    lcd.setCursor(0,1);
   }
 
-
-  lcd.setCursor(0,1);
-  switch (idt1)
+  switch (idt(idtPin))
   {
   case 0:
     lcd.print("DIST");
     // DISTANCE MODULE
     float duration, cm, inches;
-    pinMode(M1_1, OUTPUT);
-    pinMode(M1_2, INPUT);
-    digitalWrite(M1_1, LOW);
+    pinMode(gpio1, OUTPUT);
+    pinMode(gpio2, INPUT);
+    digitalWrite(gpio1, LOW);
     delayMicroseconds(5);
-    digitalWrite(M1_1, HIGH);
+    digitalWrite(gpio1, HIGH);
     delayMicroseconds(10);
-    digitalWrite(M1_1, LOW);
+    digitalWrite(gpio1, LOW);
 
-    pinMode(M1_2, INPUT);
-    duration = pulseIn(M1_2, HIGH);
+    pinMode(gpio2, INPUT);
+    duration = pulseIn(gpio2, HIGH);
 
     inches = (duration / 2) / 74;
 
@@ -183,33 +130,103 @@ void handle_modules(int tilt_x, int tilt_y, int c, int z, int idt0, int idt1) {
 
     break;
   case 1:
-    // code block
+    lcd.print("BUMPER");
+    pinMode(gpio1, INPUT);
+
+    bump = digitalRead(gpio1);
+
+    // Serial.printf("BUMP: %d \n\n", bump);
+    if (bump) {
+      // idle for half a second
+      digitalWrite(MTR1_1, LOW);
+      digitalWrite(MTR1_2, LOW);
+
+      delay(500);
+
+      // back up for 1/2 second
+      ledcWrite(steerChannel, 2000);
+      digitalWrite(MTR1_1, LOW);
+      digitalWrite(MTR1_2, HIGH);
+
+      delay(500);
+
+      // return to idle
+      digitalWrite(MTR1_1, LOW);
+      digitalWrite(MTR1_2, LOW);
+      Serial.println("STOPPPPP");
+    }
+
     break;
   case 2:
     // code block
     break;
   case 3:
+    lcd.print("TURRET");
+    ledcSetup(tilt_x_channel, freq, resolution);
+    ledcSetup(tilt_y_channel, freq, resolution);
+    ledcAttachPin(gpio0, tilt_x_channel);
+    ledcAttachPin(gpio1, tilt_y_channel);
+
+    increment_x = 0;
+    increment_y = 0;
+    if (abs(joy_x) > 10)
+    {
+      increment_x = joy_x / 2;
+    }
+    if (abs(joy_y) > 10)
+    {
+      increment_y = joy_y / 2;
+    }
+
+    turret_x -= increment_x;
+    turret_y += increment_y;
+
+    if (turret_x > turret_x_max)
+    {
+      turret_x = turret_x_max;
+    }
+    if (turret_y > turret_y_max)
+    {
+      turret_y = turret_y_max;
+    }
+    if (turret_x < turret_x_min)
+    {
+      turret_x = turret_x_min;
+    }
+    if (turret_y < turret_y_min)
+    {
+      turret_y = turret_y_min;
+    }
+
+    Serial.printf("joy_x: %d \n", joy_x);
+    Serial.printf("joy_y: %d \n", joy_y);
+
+    Serial.printf("Turret_x: %f \n", turret_x);
+    Serial.printf("Turret_y: %f \n", turret_y);
+    ledcWrite(tilt_x_channel, turret_x);
+    ledcWrite(tilt_y_channel, turret_y);
     break;
   case 4:
-    // LASER
     lcd.print("LASER");
-    pinMode(M1_0, OUTPUT);
+    // LASER
+    pinMode(gpio0, OUTPUT);
     if (c)
     {
-      digitalWrite(M1_0, HIGH);
+      digitalWrite(gpio0, HIGH);
     }
     else
     {
-      digitalWrite(M1_0, LOW);
+      digitalWrite(gpio0, LOW);
     }
     break;
   case 5:
     break;
-  case -1:
+  case 6:
+    lcd.print("NOTHING");
     Serial.println("No Module plugged in");
     break;
   default:
-    // code block
+    lcd.print("???");
     Serial.println("Unrecognized Module");
   }
 }
@@ -275,6 +292,9 @@ void setup()
   lcd.setCursor(0, 1);
   lcd.print(":)");
 
+  turret_x = 1600;
+  turret_y = 1600;
+
   Serial.print(F("Initializing Radio... "));
   // carrier frequency:           434.0 MHz
   // bandwidth:                   250.0 kHz
@@ -326,7 +346,8 @@ void loop()
            joy_x, joy_y, c, z);
 
     handle_nunchuck(tilt_x, tilt_y, z, c);
-    handle_modules(tilt_x, tilt_y, c, z, idt(IDT0), idt(IDT1));
+    handle_modules(tilt_x, tilt_y, joy_x, joy_y, c, z, IDT1, M1_0, M1_1, M1_2);
+    handle_modules(tilt_x, tilt_y, joy_x, joy_y, c, z, IDT0, M0_0, M0_1, M0_2);
   } else if (state == ERR_RX_TIMEOUT) {
     // timeout occurred while waiting for a packet
     Serial.println(F("timeout!"));
